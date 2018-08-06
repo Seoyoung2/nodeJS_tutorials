40ccbaeab06a229bec08c35c724ebe807e79891b
seoyoung <ww0111@naver.com>
2018-08-06 19:55:04 +0900

    file upload

--- app_file.js
+++ app_file.js
@@ -1,13 +1,30 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var multer = require('multer'); //파일 업로드하기 위해
+var _storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+var upload = multer({ storage: _storage })
 var fs = require('fs'); //파일시스템 제어하는 기본모듈
 var app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.locals.pretty = true;
+app.use('/user', express.static('uploads'));
 app.set('views', './views_file');
 app.set('view engine', 'pug');
 
+app.get('/upload', function(req, res){
+  res.render('upload');
+});
+app.post('/upload', upload.single('userfile'), function(req, res){
+  res.send('Uploaded : '+req.file.filename);
+});
 app.get('/topic/new', function(req, res){
   fs.readdir('data', function(err, files){
       if(err){
